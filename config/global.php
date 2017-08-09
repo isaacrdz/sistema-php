dd57ec29a344f3e040ed9a6375cf1f4eb4676b3c
isaacrdz <zack_cpp@hotmail.com>
2017-08-08 21:41:11 -0500

    sistema

---
+++ config/global.php
@@ -0,0 +1,19 @@
+<?php
+//IP de la pc servidor de la bd
+define("DB_HOST","localhost");
+
+//nombre de la base de datos
+define("DB_NAME","dbsistema");
+
+//usuario de la base de datos
+define("DB_USERNAME","sistemauser2");
+
+// password
+define("DB_PASSWORD","z8034760");
+
+//codificacion de los caracteres
+define("DB_ENCODE","utf8");
+
+//nombre de la base de datos
+define("PRO_NOMBRE","ITventas");
+ ?>
